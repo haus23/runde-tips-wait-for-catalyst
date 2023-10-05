869cedac80785bbd4db62d1aaacfc980bb29644a
Micha Buchholz <micha@haus23.net>
2023-10-05 13:19:07 +0200

    chore(dx): Add and configure tailwind.

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,8 @@
-import { LiveReload, Outlet } from '@remix-run/react';
+import type { LinksFunction } from '@remix-run/node';
+import { Links, LiveReload, Outlet } from '@remix-run/react';
+
+import styles from '~/styles/tailwind.css';
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
 
 export default function App() {
   return (
@@ -7,6 +11,7 @@ export default function App() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>runde.tips</title>
+        <Links />
       </head>
       <body>
         <Outlet />
--- app/routes/_app+/_layout.tsx
+++ app/routes/_app+/_layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from '@remix-run/react';
 export default function AppLayout() {
   return (
     <div>
-      <h1>runde.tips</h1>
+      <h1 className="text-2xl font-semibold">runde.tips</h1>
       <Outlet />
     </div>
   );
