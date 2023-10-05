7887e1e8d4b380bf88a820faa9a0c862516c4836
Micha Buchholz <micha@haus23.net>
2023-10-05 12:36:37 +0200

    feat: Display current championship.

--- app/routes/_index/_standings.tsx
+++ app/routes/_index/_standings.tsx
@@ -1,3 +1,28 @@
+import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+
+import { db } from '~/utils/server/db.server';
+
+export async function loader() {
+  const championship = await db.championship.findFirst({
+    orderBy: { nr: 'desc' },
+    where: { published: true },
+  });
+
+  if (!championship) {
+    throw new Error('No championship found');
+  }
+
+  return json(championship);
+}
+
 export default function StandingsRoute() {
-  return <h2>Tabelle</h2>;
+  const championship = useLoaderData<typeof loader>();
+
+  return (
+    <h2>
+      {championship.completed ? 'Abschlusstabelle' : 'Aktuelle Tabelle'}{' '}
+      {championship.name}
+    </h2>
+  );
 }
