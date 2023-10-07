fd17e2709f36a95db0f287dde8d7dc7a447732a3
Micha Buchholz <micha@haus23.net>
2023-10-08 00:03:07 +0200

    feat: Add desktop nav.

--- app/routes/_foh+/header.tsx
+++ app/routes/_foh+/header.tsx
@@ -1,9 +1,11 @@
+import { NavDesktop } from './nav-desktop';
 import { NavMobile } from './nav-mobile';
 
 export function Header() {
   return (
     <header className="h-16 border-b shadow px-2 sm:px-4 bg-bg-subtle">
       <NavMobile />
+      <NavDesktop />
     </header>
   );
 }
