c682965220dc4109aae12c9289d42b725cd4953d
Micha Buchholz <micha@haus23.net>
2023-10-07 23:14:26 +0200

    feat: Add mobile nav.

---
+++ app/routes/_foh+/header.tsx
@@ -0,0 +1,9 @@
+import { NavMobile } from './nav-mobile';
+
+export function Header() {
+  return (
+    <header className="h-16 border-b shadow px-2 sm:px-4 bg-bg-subtle">
+      <NavMobile />
+    </header>
+  );
+}
