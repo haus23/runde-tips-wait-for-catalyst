7c547e013f6d1f253e96f8fee522eea3c620c498
Micha Buchholz <micha@haus23.net>
2023-10-12 19:49:18 +0200

    chore(dx): Add local prettier and configure imports sorting. Resolves #5

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/** @type {import('prettier').Config} */
+/** @type {import('@ianvs/prettier-plugin-sort-imports').PrettierConfig} */
+export default {
+  singleQuote: true,
+  importOrder: [
+    '^react$',
+    '^@remix-run/',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '#',
+    '',
+    '^[.]',
+  ],
+  importOrderTypeScriptVersion: '5.2.0',
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+};
